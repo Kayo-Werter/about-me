9aedb1d2dcd8f929c737cb95edafb224d37ef893
Kayo Werter <kayo.werter1@gmail.com>
2023-09-24 23:15:20 -0300

    fim

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-const chk = document.getElementById('chk')
-const html = document.documentElement;
-
- 
-
-chk.addEventListener('change', () => {
-  html.classList.toggle('light')
-})
-
+function ativarLightMode() {
+  const chk = document.getElementById('chk')
+  const html = document.documentElement;
+  chk.addEventListener('change', () => {
+    html.classList.toggle('light')
+  })
+}
+ativarLightMode()

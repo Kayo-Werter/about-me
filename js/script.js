588089e71bb498814cd16c39b3f44927c524dbb8
Kayo Werter <kayo.werter1@gmail.com>
2023-09-24 23:06:18 -0300

    atualizacao light mode

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 const chk = document.getElementById('chk')
-const linkNavegacao = document.querySelectorAll('.apresentacao__links__navegacao')
+const html = document.documentElement;
 
  
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-  linkNavegacao.forEach(x => x.classList.toggle('dark'));
+  html.classList.toggle('light')
 })
 
